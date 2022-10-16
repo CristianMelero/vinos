ad9dabfe00704ed2b7c92574b8f3d886cb3f1411
Cristian Melero <cristianmelero22@yahoo.com.ar>
2022-10-15 21:26:22 -0300

    MENU HAMBURGUESA LISTO

--- src/main.js
+++ src/main.js
@@ -48,3 +48,55 @@ const paintData = (data) => {
 const buyButton = () => {
     alert("Gracias por la compra")
 }
+
+//-------------HEADER--------------//
+document.getElementById("idheader").innerHTML = `
+<div class="header-logo">
+  <a href="index.html">
+    <img src="Images/gg-logo.svg" alt="Logo GG Wines" width="100">
+  </a>
+</div>
+<div class="header-title">
+  <p>GG Wines</p>
+</div>
+<input type="checkbox" id="menu-check">
+<label id="menu" for="menu-check">
+  <span id="menu-open">&#9776;</span>
+  <span id="menu-close">X</span>
+</label>
+<nav class="header-navbar">
+  <ul>
+    <li><a href="index.html">Inicio</a></li>
+    <li><a href="quienesSomos.html">Sobre nosotros</a></li>
+    <li><a href="catas.html">Catas</a></li>
+    <li><a href="tienda.html">Catálogo</a></li>
+    <li><a href="contacto.html">Contactanos</a></li> 
+  </ul>
+</nav>
+`
+//-------------FOOTER--------------//
+document.getElementById("idfooter").innerHTML = `
+<div class="row">
+<div class="footer-text"> <h2>Contactanos |</h2> </div>
+<div>
+  <a href="index.html">
+    <img src="./Icons/whatsapp.png"> 
+  </a>
+</div>
+<div>
+  <a href="index.html">
+    <img src="./Icons/instagram.png"> 
+  </a>
+</div>
+<div>
+  <a href="index.html">
+    <img src="./Icons/twitter.png"> 
+  </a>
+</div>
+<div>
+  <a href="index.html">
+    <img src="./Icons/gmail.png"> 
+  </a>
+</div>
+</div>
+`
